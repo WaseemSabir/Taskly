9210cf8c6c5fb2ed49ddc3fddd80327192ba95c4
Waseem Sabir <waseem.sabir@tajir-app.com>
2022-05-23 13:12:37 +0500

    Checkpoint 1 added

---
+++ Taskly/app/src/main/java/com/tajir/taskly/viewModels/UserStateViewModel.kt
@@ -0,0 +1,14 @@
+package com.tajir.taskly.viewModels
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+
+class UserStateViewModel : ViewModel() {
+    var isLoggedIn by mutableStateOf(false)
+}
+
+val UserState = compositionLocalOf<UserStateViewModel> { error("User State Context Not Found!") }
